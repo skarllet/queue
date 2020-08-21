7076cc302739f89a09b147730b5f6c1504485895
João Scheuermann <joao.scheuermann@xpi.com.br>
2020-08-21 12:49:44 -0300

    Removed the event registration

--- src/index.js
+++ src/index.js
@@ -7,29 +7,23 @@ const clearArray = array => {
 }
 
 module.exports = {
-  create: () => {
+  create: (handler = async () => {}) => {
     const { emmit, on } = e.create()
 
-    const events = {}
     const queue = []
 
     const next = () => {
-      if (!queue.length) return // ends the loop
+      if (!queue.length) {
+        emmit('finished')
+        return
+      }
 
       try {
-        const { event, params } = queue.shift()
+        const item = queue.shift()
 
-        emmit('next', { event, params })
+        emmit('next', item)
 
-        const current = events[event]
-
-        if (current === undefined)
-          throw new Error(`Seems like the event '${event}' doesn't exists. Did you register it?`)
-
-        if (typeof current !== 'function' || current.constructor.name !== "AsyncFunction")
-          throw new Error(`Seems like the handler for the event '${event}' it's not a function or a Promise. Did you register it correctly?`)
-
-        current(params)
+        handler(params)
           .then(() => next())
           .catch(error => emmit('error', error))
 
@@ -38,28 +32,21 @@ module.exports = {
       }
     }
 
-    const push = (event = '', params = null) => queue.push({event, params})
+    const push = (event = '', payload = null) => queue.push({event, payload})
 
     const start = () => next()
 
     const clear = () => clearArray(queue)
-
-    const register = object => Object.entries(object).map(([key, value]) => events[key] = value)
 
     return {
       on,
       push,
       start,
       clear,
-      register,
 
       get current() {
         return [ ...queue ]
       },
-
-      get events() {
-        return { ...events }
-      }
     }
   }
 }
